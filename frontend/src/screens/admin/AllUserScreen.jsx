24ac6988018eee045b2ac9bb9fc9990c4b0fe9c6
irtiza1999 <mdirtiza.hossain199@gmail.com>
2023-08-19 21:37:46 +0600

    some fix

--- frontend/src/screens/admin/AllUserScreen.jsx
+++ frontend/src/screens/admin/AllUserScreen.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from 'react';
 import { Typography, Table, TableHead, TableBody, TableRow, TableCell, Paper } from '@material-ui/core';
-import { useGetAllUsersQuery, useMakeAdminMutation, useRemoveAdminMutation, 
-    useUpdateUserMutation,useRemoveUserMutation } from '../../slices/userApiSlice.js';
+import {
+  useGetAllUsersQuery, useMakeAdminMutation, useRemoveAdminMutation,
+  useUpdateUserMutation, useRemoveUserMutation
+} from '../../slices/userApiSlice.js';
 import Loader from '../../components/Loader.jsx';
 import Message from '../../components/Message.jsx';
 import { LinkContainer } from 'react-router-bootstrap';
@@ -12,20 +14,20 @@ import { toast } from 'react-toastify';
 import { useSelector } from 'react-redux';
 
 const AllUserScreen = () => {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [userId, setUserId] = useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [userId, setUserId] = useState('');
   const { userInfo } = useSelector((state) => state.auth);
   const { data: users, refetch, isLoading, error } = useGetAllUsersQuery();
   const [makeAdmin, adminIsLoading] = useMakeAdminMutation();
   const [removeAdmin, removeAdminLoading] = useRemoveAdminMutation();
   const [updateUser, updateUserLoading] = useUpdateUserMutation();
-  const [updateProfile, {profileIsLoading}] = useUpdateUserMutation();
+  const [updateProfile, { profileIsLoading }] = useUpdateUserMutation();
   const [removeUser, removeUserLoading] = useRemoveUserMutation();
 
-  const [show, setShow] = useState(false);  
+  const [show, setShow] = useState(false);
 
   const handleClose = () => {
     setShow(false);
@@ -33,6 +35,7 @@ const AllUserScreen = () => {
     setName('');
     setEmail('');
     setPassword('');
+    setConfirmPassword('');
   };
 
   const handleShow = (user) => {
@@ -61,9 +64,9 @@ const AllUserScreen = () => {
 
   const handleRemoveFromAdmin = async (userId, name) => {
     if (userInfo._id === userId) {
-        toast.error('You cannot remove yourself from admin');
-        return;
-      }
+      toast.error('You cannot remove yourself from admin');
+      return;
+    }
     const res = await removeAdmin({ userId });
     if (res.error) toast.error(res.error.message);
     else {
@@ -90,31 +93,32 @@ const AllUserScreen = () => {
       toast.error(error?.data?.message || error?.error);
     }
   };
-  
+
   const submitHandler = async (e) => {
     e.preventDefault();
-    if (password != confirmPassword){
-        toast.error('Passwords do not match');
+    if (password != confirmPassword) {
+      toast.error('Passwords do not match');
     }
-    else{
-      if (password.length <= 0){
+    else {
+      if (password.length <= 0) {
         toast.error('Please enter your password');
-    }else{
-        try{
-            const res = await updateProfile({
-              _id: userId,
-              name,
-              email,
-              password
-            }).unwrap();
-            handleClose();
-            refetch();
-            toast.success('Profile Updated Successfully');
-        }catch(err){
-            toast.error(err?.data?.message || err?.error);
-        }}
+      } else {
+        try {
+          const res = await updateProfile({
+            _id: userId,
+            name,
+            email,
+            password
+          }).unwrap();
+          handleClose();
+          refetch();
+          toast.success('Profile Updated Successfully');
+        } catch (err) {
+          toast.error(err?.data?.message || err?.error);
+        }
+      }
     }
-};
+  };
 
   return (
     <Grid container spacing={2}>
@@ -122,7 +126,7 @@ const AllUserScreen = () => {
         <AdminPanelScreen />
       </Grid>
       <Grid item xs={10}>
-        <Typography variant="h3" style={{ margin : '10px'}}>All Customers</Typography>
+        <Typography variant="h3" style={{ margin: '10px' }}>All Customers</Typography>
         {isLoading ? (
           <Loader />
         ) : error ? (
@@ -162,23 +166,23 @@ const AllUserScreen = () => {
                       <TableCell>{user.email}</TableCell>
                       <TableCell>
                         {user.isAdmin ? (
-                        <Button variant="danger"
-                        className="btn-sm"
-                        onClick={() => handleRemoveFromAdmin(user._id, user.name)} 
-                        style={{ width: '150px' }}>
+                          <Button variant="danger"
+                            className="btn-sm"
+                            onClick={() => handleRemoveFromAdmin(user._id, user.name)}
+                            style={{ width: '150px' }}>
                             Remove From Admin
-                        </Button>
+                          </Button>
                         ) : (
-                        <Button variant="success" className="btn-sm"
-                        onClick={() => handleMakeAdmin(user._id, user.name)} 
-                        style={{ width: '150px' }}>
+                          <Button variant="success" className="btn-sm"
+                            onClick={() => handleMakeAdmin(user._id, user.name)}
+                            style={{ width: '150px' }}>
                             Make Admin
-                        </Button>
+                          </Button>
                         )}
                       </TableCell>
                       <TableCell>
-                        <Button variant="info" onClick={() => handleShow(user)}  className="btn-sm"
-                        style={{ width: '150px' }}>
+                        <Button variant="info" onClick={() => handleShow(user)} className="btn-sm"
+                          style={{ width: '150px' }}>
                           Update User
                         </Button>
                       </TableCell>
@@ -194,33 +198,33 @@ const AllUserScreen = () => {
           <Modal.Title>Update User</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-        <Form onSubmit={submitHandler}>
-        <Form.Group className = 'my-2' controlId='name'>
-            <Form.Label>Enter Name</Form.Label>
-            <Form.Control type='text' placeholder='Enter Name' value={name} onChange={(e) => setName(e.target.value)}></Form.Control>
-        </Form.Group>
+          <Form onSubmit={submitHandler}>
+            <Form.Group className='my-2' controlId='name'>
+              <Form.Label>Enter Name</Form.Label>
+              <Form.Control type='text' placeholder='Enter Name' value={name} onChange={(e) => setName(e.target.value)}></Form.Control>
+            </Form.Group>
 
-        <Form.Group className = 'my-2' controlId='email'>
-            <Form.Label>Email Address</Form.Label>
-            <Form.Control type='email' placeholder='Enter Email' value={email} onChange={(e) => setEmail(e.target.value)}></Form.Control>
-        </Form.Group>
+            <Form.Group className='my-2' controlId='email'>
+              <Form.Label>Email Address</Form.Label>
+              <Form.Control type='email' placeholder='Enter Email' value={email} onChange={(e) => setEmail(e.target.value)}></Form.Control>
+            </Form.Group>
 
-        <Form.Group className = 'my-2' controlId='password'>
-            <Form.Label>Update Password {' '}
+            <Form.Group className='my-2' controlId='password'>
+              <Form.Label>Update Password {' '}
                 <small>(If you don't want to change password just retype the old password)</small></Form.Label>
-            <Form.Control type='password' placeholder='Update Password' value={password} onChange={(e) => setPassword(e.target.value)}></Form.Control>
-        </Form.Group>
+              <Form.Control type='password' placeholder='Update Password' value={password} onChange={(e) => setPassword(e.target.value)}></Form.Control>
+            </Form.Group>
 
-        <Form.Group className = 'my-2' controlId='cPassword'>
-            <Form.Label>Confirm Updated Password</Form.Label>
-            <Form.Control type='password' placeholder='Confirm Updated Password' value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}></Form.Control>
-        </Form.Group>
-        {isLoading && <Loader />}
-        <Button type='submit' variant='primary' className='mt-3 btn-sm'>Update Profile</Button>
-            </Form>
+            <Form.Group className='my-2' controlId='cPassword'>
+              <Form.Label>Confirm Updated Password</Form.Label>
+              <Form.Control type='password' placeholder='Confirm Updated Password' value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}></Form.Control>
+            </Form.Group>
+            {isLoading && <Loader />}
+            <Button type='submit' variant='primary' className='mt-3 btn-sm'>Update Profile</Button>
+          </Form>
         </Modal.Body>
         <Modal.Footer>
-        <Button className="btn-sm" variant='danger' onClick={handleRemoveUser}>Remove User</Button>
+          <Button className="btn-sm" variant='danger' onClick={handleRemoveUser}>Remove User</Button>
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
